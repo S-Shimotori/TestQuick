ea2098d39e7ca6528a4b318bd00f5624a81d89f0
Shigure Shimotori <komata.h.aa@m.titech.ac.jp>
2015-07-06 12:13:13 +0900

    add test
    Documentation/ArrangeActAssert.md

--- Swift2.0/TestQuick/TestQuick/Offer.swift
+++ Swift2.0/TestQuick/TestQuick/Offer.swift
@@ -7,3 +7,11 @@
 //
 
 import Foundation
+
+public func offer(banana: Banana) -> String {
+    if banana.isEdible {
+        return "Hey, want a banana?"
+    } else {
+        return "Hey, want me to peel this banana for you?"
+    }
+}
---
+++ Swift2.0/TestQuick/TestQuickTests/OfferTests.swift
@@ -0,0 +1,52 @@
+//
+//  OfferTests.swift
+//  TestQuick
+//
+//  Created by S-Shimotori on 7/6/15.
+//  Copyright © 2015 S-Shimotori. All rights reserved.
+//
+
+import XCTest
+
+class OfferTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testOffer_whenTheBananaIsPeeled_offersTheBanana() {
+        let banana = Banana()
+        banana.peel()
+
+        let message = offer(banana)
+
+        XCTAssertEqual(message, "Hey, want a banana?")
+    }
+
+    func testOffer_whenTheBananaIsntPeeled_offersToPeelTheBanana() {
+        let banana = Banana()
+
+        let message = offer(banana)
+
+        XCTAssertEqual(message, "Hey, want me to peel this banana for you?")
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
